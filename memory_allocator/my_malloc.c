4901312cf559de1858db4d3b517b65d8e4055478
Christophe Kafrouni <ckafrouni@proton.me>
2023-10-11 15:16:38 +0000

    Adding function to display heap

--- memory_allocator/my_malloc.c
+++ memory_allocator/my_malloc.c
@@ -23,6 +23,44 @@
 
 uint8_t MY_HEAP[64000];
 
+void print_all_blocks()
+{
+    const uint16_t METADATA_SIZE = 2;
+    uint16_t *start = (uint16_t *)MY_HEAP;
+    uint16_t *current = (uint16_t *)(MY_HEAP + METADATA_SIZE); // Skip the initial "start" pointer
+
+    printf("\033[01m=== HEAP STATE ===\n");
+    printf("heap address = %p\n", MY_HEAP);
+    printf("1st free block's offset = %u\n", *start);
+    printf("------------------\n");
+
+    int num_block = 0;
+    while ((uint8_t *)current < MY_HEAP + sizeof(MY_HEAP))
+    {
+        uint16_t metadata = *current;
+        uint16_t size = metadata & ~1;   // Mask out the lowest bit to get the size
+        int is_allocated = metadata & 1; // Check the lowest bit to see if it's allocated
+        uint16_t offset = (uint8_t *)current - (uint8_t *)MY_HEAP;
+
+        printf("\033[31;1m#%d\033[0m\t", num_block);
+        if (is_allocated)
+        {
+            printf("\033[33;1mallocated\toffset = %u\t%%p = %p\tsize = %u\n\033[0m", offset, current, size);
+        }
+        else
+        {
+            uint16_t next_offset = *(current + 1);
+            uint16_t prev_offset = *(current + 2);
+            printf("\033[32;1mfree\t\toffset = %u\t%%p = %p\tsize = %u\tnext_offset = %u\tprev_offset = %u\n\033[0m", offset, current, size, next_offset, prev_offset);
+        }
+
+        num_block++;
+        current += size / METADATA_SIZE; // Move to the next block based on the size
+    }
+
+    printf("\033[1m==================\n\033[0m");
+}
+
 void my_init()
 {
     const uint16_t METADATA_SIZE = 2;
@@ -135,13 +173,19 @@ void my_free(void *pointer)
 int main()
 {
     my_init();
+    print_all_blocks();
     char *p = (char *)my_malloc(5);
+    print_all_blocks();
     strcpy(p, "Hello");
-    printf("p: %s\n", p);
+    printf("\np: %s\n\n", p);
 
+    print_all_blocks();
     char *q = (char *)my_malloc(6);
     strcpy(q, "world");
-    printf("q: %s\n", q);
+    printf("\nq: %s\n\n", q);
+
+    my_init();
+    print_all_blocks();
 
     return 0;
 }
