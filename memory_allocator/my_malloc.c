36f067f716bb364738e05161326dd4679846918a
Christophe Kafrouni <ckafrouni@proton.me>
2023-10-11 15:38:57 +0000

    Added colors to printer

--- memory_allocator/my_malloc.c
+++ memory_allocator/my_malloc.c
@@ -29,10 +29,10 @@ void print_all_blocks()
     uint16_t *start = (uint16_t *)MY_HEAP;
     uint16_t *current = (uint16_t *)(MY_HEAP + METADATA_SIZE); // Skip the initial "start" pointer
 
-    printf("\033[01m=== HEAP STATE ===\n");
+    printf("\033[34;1m=== HEAP STATE ===\n\033[0;1m");
     printf("heap address = %p\n", MY_HEAP);
     printf("1st free block's offset = %u\n", *start);
-    printf("------------------\n");
+    printf("\033[34m------------------\n");
 
     int num_block = 0;
     while ((uint8_t *)current < MY_HEAP + sizeof(MY_HEAP))
@@ -42,23 +42,23 @@ void print_all_blocks()
         int is_allocated = metadata & 1; // Check the lowest bit to see if it's allocated
         uint16_t offset = (uint8_t *)current - (uint8_t *)MY_HEAP;
 
-        printf("\033[31;1m#%d\033[0m\t", num_block);
+        printf("\033[31m#%d\033[0;1m\t", num_block);
         if (is_allocated)
         {
-            printf("\033[33;1mallocated\toffset = %u\t%%p = %p\tsize = %u\n\033[0m", offset, current, size);
+            printf("\033[33mallocated\toffset = %u\t%%p = %p\tsize = %u\tpayload size = %u\tcontent: \"%s\"\n\033[0;1m", offset, current, size, size - 2 * METADATA_SIZE, (char *)(current + 1));
         }
         else
         {
             uint16_t next_offset = *(current + 1);
             uint16_t prev_offset = *(current + 2);
-            printf("\033[32;1mfree\t\toffset = %u\t%%p = %p\tsize = %u\tnext_offset = %u\tprev_offset = %u\n\033[0m", offset, current, size, next_offset, prev_offset);
+            printf("\033[32mfree\t\toffset = %u\t%%p = %p\tsize = %u\tnext offset = %u\tprev offset = %u\n\033[0;1m", offset, current, size, next_offset, prev_offset);
         }
 
         num_block++;
         current += size / METADATA_SIZE; // Move to the next block based on the size
     }
 
-    printf("\033[1m==================\n\033[0m");
+    printf("\033[34m==================\n\033[0m");
 }
 
 void my_init()
@@ -174,15 +174,19 @@ int main()
 {
     my_init();
     print_all_blocks();
+
     char *p = (char *)my_malloc(5);
     print_all_blocks();
     strcpy(p, "Hello");
-    printf("\np: %s\n\n", p);
-
     print_all_blocks();
+
     char *q = (char *)my_malloc(6);
-    strcpy(q, "world");
-    printf("\nq: %s\n\n", q);
+    strcpy(q, "world!");
+    print_all_blocks();
+
+    char *pq = (char *)my_malloc(11);
+    sprintf(pq, "%s %s", p, q);
+    print_all_blocks();
 
     my_init();
     print_all_blocks();
