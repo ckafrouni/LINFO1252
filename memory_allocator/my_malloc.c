2c1797aba2ec18668d3b42ec9f4b8cba62ddd442
Christophe Kafrouni <ckafrouni@proton.me>
2023-10-11 16:19:07 +0000

    setup

--- memory_allocator/my_malloc.c
+++ memory_allocator/my_malloc.c
@@ -6,60 +6,7 @@
 
 #include "my_malloc.h"
 
-/**
- * This heap is build as a doubly linked, explicit list.
- *
- * Explicit list, meaning each free block points towards the next free block.
- * Unlike implicit list, where every block points to the next, wether it is free, or not.
- *
- * Doubly linked, meaning each block starts and ends with a metadata word. Allowing traversing both ways.
- * Free block contain on top of that, two extra words, one containing a offset to the next free block, and
- * another containing a offset towards the previous free block.
- * --
- * Our heap size is 64 [kb], and we chose 2 [byte] length words.
- * --
- * The heap starts with a word that points towards the first free block.
- */
-
 uint8_t MY_HEAP[64000];
-
-void print_all_blocks()
-{
-    const uint16_t METADATA_SIZE = 2;
-    uint16_t *start = (uint16_t *)MY_HEAP;
-    uint16_t *current = (uint16_t *)(MY_HEAP + METADATA_SIZE); // Skip the initial "start" pointer
-
-    printf("\033[34;1m=== HEAP STATE ===\n\033[0;1m");
-    printf("heap address = %p\n", MY_HEAP);
-    printf("1st free block's offset = %u\n", *start);
-    printf("\033[34m------------------\n");
-
-    int num_block = 0;
-    while ((uint8_t *)current < MY_HEAP + sizeof(MY_HEAP))
-    {
-        uint16_t metadata = *current;
-        uint16_t size = metadata & ~1;   // Mask out the lowest bit to get the size
-        int is_allocated = metadata & 1; // Check the lowest bit to see if it's allocated
-        uint16_t offset = (uint8_t *)current - (uint8_t *)MY_HEAP;
-
-        printf("\033[31m#%d\033[0;1m\t", num_block);
-        if (is_allocated)
-        {
-            printf("\033[33mallocated\toffset = %u\t%%p = %p\tsize = %u\tpayload size = %u\tcontent: \"%s\"\n\033[0;1m", offset, current, size, size - 2 * METADATA_SIZE, (char *)(current + 1));
-        }
-        else
-        {
-            uint16_t next_offset = *(current + 1);
-            uint16_t prev_offset = *(current + 2);
-            printf("\033[32mfree\t\toffset = %u\t%%p = %p\tsize = %u\tnext offset = %u\tprev offset = %u\n\033[0;1m", offset, current, size, next_offset, prev_offset);
-        }
-
-        num_block++;
-        current += size / METADATA_SIZE; // Move to the next block based on the size
-    }
-
-    printf("\033[34m==================\n\033[0m");
-}
 
 void my_init()
 {
@@ -168,28 +115,4 @@ void my_free(void *pointer)
     //      else
     //          Point the 'next pointer' to the head of next
     //          Change 'previous pointer' of next to the head of current/previous of current
-}
-
-int main()
-{
-    my_init();
-    print_all_blocks();
-
-    char *p = (char *)my_malloc(5);
-    print_all_blocks();
-    strcpy(p, "Hello");
-    print_all_blocks();
-
-    char *q = (char *)my_malloc(6);
-    strcpy(q, "world!");
-    print_all_blocks();
-
-    char *pq = (char *)my_malloc(11);
-    sprintf(pq, "%s %s", p, q);
-    print_all_blocks();
-
-    my_init();
-    print_all_blocks();
-
-    return 0;
 }
--- memory_allocator/my_malloc.h
+++ memory_allocator/my_malloc.h
@@ -2,9 +2,12 @@
 #define MY_MALLOC_H
 
 #include <stddef.h>
+#include <stdint.h>
+
+extern uint8_t MY_HEAP[64000];
 
 // Initialize the memory allocator
-void init();
+void my_init();
 
 // Allocate a block of memory of the given size
 void *my_malloc(size_t size);
